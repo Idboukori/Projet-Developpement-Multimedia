f0e2f26618cfd0a069ea111edbfa5afca163a309
IDBOUKORI Ali <idboukori@gmail.com>
2019-04-19 03:57:58 +0200

    new version2

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ var canvas ;
 var btnCapture;
 var imcanvas;
 var captureFlag = false;
-
-
+var btnConvert;
 function watch_video(){
+    
 
     document.getElementById("divs").style.display = "block";
-    
+    document.getElementById("conv").style.display ="none";
     MyCam = document.getElementById("MyCam");
-    canvas = document.getElementById("canvas");
+    canvas = document.getElementById('canvas');
     btnCapture = document.getElementById("btnCapture");
-
+    btnConvert= document.getElementById("btnConvert")
     imcanvas = canvas.getContext("2d");
 
-    btnCapture.addEventListener("click", capture);
-
+    btnCapture.addEventListener("click",capture);
+    btnConvert.addEventListener("click",convert);
 
     navigator.getUserMedia = (
 
@@ -56,19 +56,30 @@ function capture(){
     
     var seconds = 4;
     var countdown = setInterval(function() {
-        seconds--;
+         seconds--;
         document.getElementById("btnCapture").textContent = seconds;
+       
         if (seconds <= 0){
             clearInterval(countdown);
             captureFlag = true;
         
     imcanvas.drawImage(MyCam, 0, 0, canvas.width, canvas.height);
+    
+    
     document.getElementById("btnCapture").textContent="capturer";
+
+
         } 
     }, 1000);
     
     
 }
 
+function convert(){
+    document.getElementById("conv").style.display ="block";
+    var base64 = canvas.toDataURL("image/jpeg");	//l'image au format base 64
+    document.getElementById('conv').value = '';
+    document.getElementById('conv').value = base64;
+}
 
 
